1534d9a8265e27bc069776ac7b2b8744acd07d3b
bigbyto <bigbyto@gmail.com>
2023-06-15 12:55:53 +0800

    chore: add assert

--- src/configuration.py
+++ src/configuration.py
@@ -10,7 +10,10 @@ class Configuration():
     self.serverPort = os.getenv(ENV_PORT) or config.get('server_port',8010)
     self.assetsDir = os.getenv(ENV_ASSETS_DIR) or config.get('assets_dir')
     self.appKey = config.get('app_key')
+    assert self.appKey, 'app_key must not be empty'
     self.datasource = config.get('datasource')
     self.mode = config.get('mode')
+    assert self.mode in ['select','load-balance'], 'mode must be select or load-balance'
     self.loadbalanceRule = config.get('load-balance-rule')
     self.services = config.get('services')
+    assert len(self.services) > 0, 'services must not be empty'
